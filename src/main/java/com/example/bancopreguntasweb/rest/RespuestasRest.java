ca89a79fab553094b7e22e6799d56afb92ddc3cc
Bryant Portilla <bryantporti@gmail.com>
2021-01-31 23:08:39 -0500

    dsad

--- src/main/java/com/example/bancopreguntasweb/rest/RespuestasRest.java
+++ src/main/java/com/example/bancopreguntasweb/rest/RespuestasRest.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.BeaconUtpl.Models.Usuario;
 import com.example.bancopreguntasweb.models.entities.Respuestas;
 import com.example.bancopreguntasweb.models.services.implementation.BancoPreguntasServiceImpl;
 import com.example.bancopreguntasweb.models.services.implementation.RespuestasServiceImpl;
